3174205f80e711e990d8ce028d8096a94bf512ce
philipsz-davido <philipsz-davido>
2017-11-18 12:36:51 +0100

    Did some stuff

--- server/boot/create-data-tables.js
+++ server/boot/create-data-tables.js
@@ -3,19 +3,27 @@ module.exports = function(app) {
         if (err) throw err;
 
         app.models.Albums.create([{
-            name: 'Luke Skywalker',
-            artists: [{}],
+            name: 'Arrival',
+            artists: [{ name: 'Abba' }],
             album_type: 'Pop',
-            image: 'Male',
-            tracks: [{}],
-            release_date: "2017"
+            image: 'http://www.softshoe-slim.com/covers2/a/abba04.jpg',
+            tracks: [{
+                name: 'Fernando',
+                image: 'http://images.eil.com/large_image/ABBA_FERNANDO-551337.jpg',
+                duration: 8989
+            }],
+            release_date: "1976"
         }, {
-            name: 'C-3PO',
-            artists: [{}],
+            name: 'Thriller',
+            artists: [{ name: 'Michael Jackson' }],
             album_type: 'Pop',
-            image: 'Male',
-            tracks: [{}],
-            release_date: "2017"
+            image: 'https://upload.wikimedia.org/wikipedia/en/8/89/Michael_jackson_thriller_12_inch_single_USA.jpg',
+            tracks: [{
+                name: 'Billie Jean',
+                image: 'https://orig00.deviantart.net/cb71/f/2011/050/c/1/michael_jackson___billie_jean_by_prozdesign-d39xo4g.jpg',
+                duration: 8989,
+            }],
+            release_date: "1982"
         }], function(err, Artists) {
             if (err) throw err;
 
@@ -27,17 +35,17 @@ module.exports = function(app) {
         if (err) throw err;
 
         app.models.Tracks.create([{
-            name: 'A New Hope',
-            image: 'It is a period of civil war',
+            name: 'Fernando',
+            image: 'http://images.eil.com/large_image/ABBA_FERNANDO-551337.jpg',
             duration: 8989,
-            albums: [{}],
-            artists: [{}]
+            albums: [{ name: 'Arrival' }],
+            artists: [{ name: 'Abba' }]
         }, {
-            name: 'Chidume Nnamdi',
-            image: 'It is a period of civil warr',
-            duration: 989,
-            albums: [{}],
-            artists: [{}]
+            name: 'Billie Jean',
+            image: 'https://orig00.deviantart.net/cb71/f/2011/050/c/1/michael_jackson___billie_jean_by_prozdesign-d39xo4g.jpg',
+            duration: 9894,
+            albums: [{ name: 'Thriller' }],
+            artists: [{ name: 'Michael Jackson' }]
         }], function(err, Tracks) {
             if (err) throw err;
 
@@ -49,15 +57,15 @@ module.exports = function(app) {
         if (err) throw err;
 
         app.models.Artists.create([{
-            name: 'Death Star',
-            popularity: 5,
-            genres: [{ name: 'Imperial Department of Military Research' }],
-            image: '843342'
+            name: 'Abba',
+            popularity: 100,
+            genres: [{ name: 'Pop' }],
+            image: 'https://upload.wikimedia.org/wikipedia/en/2/27/ABBA_-_The_Album_%28Polar%29.jpg'
         }, {
-            name: 'Sentinel-class landing craft',
-            popularity: 90,
-            genres: [{ name: 'Imperial Department of Military Research' }],
-            image: '75'
+            name: 'Michael Jackson',
+            popularity: 100,
+            genres: [{ name: 'Pop' }],
+            image: 'https://i.ebayimg.com/images/a/T2eC16JHJGIE9nnWphDbBQdqYRwKD!~~/s-l300.jpg'
         }], function(err, Albums) {
             if (err) throw err;
 
